15ba87471645670403f1f095b590531e7c5ec84c
Erik Bodzsar <erikbodzsar@gmail.com>
2018-02-12 21:54:20 -0800

    Date testing

--- main.js
+++ main.js
@@ -1206,6 +1206,8 @@ fillModsTable = function() {
   allHeaders.select(".modName").select("input").attr("disabled", function(d) { return 'auto' in d[1] ? "" : null; });
   allHeaders.select(".modDate").attr("value", function(d) { return yyyyMmDd(d[0]); });
   allHeaders.select(".modDate").property("value", function(d) { return yyyyMmDd(d[0]); });
+  allHeaders.select(".modDate").attr("value", function(d) { return "2015-02-02"; }); // xxx
+  allHeaders.select(".modDate").property("value", function(d) { return "2015-02-02"; }); // xxx
   allHeaders.select(".modDate").attr("disabled", function(d) { return 'auto' in d[1] ? "" : null; });
   allHeaders.select(".modDate").nodes().forEach(function(n) { var disp = n.style.display; n.style.display = "none"; var x = n.offsetHeight; n.style.display = disp; }); // xxx
   portfolioTable(allModContainers.select(".portfolioBody"), function(d) { return getPortfolio(d[0]); },
